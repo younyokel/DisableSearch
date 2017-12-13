3a367bd72b92e03f62e4df088ad31a783089b443
freeroot <free.r@gmx.fr>
2017-12-14 00:15:28 +0100

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# Disable Search
+It's a GNOME extension.
+
+# Description / Effect
+Hide the search entry from the overview and disable associated search.
+
+# How-to install
+https://extensions.gnome.org/extension/203/disable-search/
+
+# Compatibility
+Version of GNOME :
+
+    · 3.26
+
+It should work with any version of GNOME 3.
+Don't hesitate to tell me if it works with a particular version of GNOME, I will add it officially.
---
+++ extension.js
@@ -0,0 +1,21 @@
+const Main = imports.ui.main;
+
+let SavedstartSearch;
+let SavedonTextChanged;
+
+function init() {
+	this.SavedstartSearch = Main.overview._controls.viewSelector.startSearch;
+	this.SavedonTextChanged = Main.overview._controls.viewSelector._onTextChanged;
+}
+
+function enable() {
+	Main.overview._searchEntry.hide();
+	Main.overview._controls.viewSelector.startSearch = function(event) {};
+	Main.overview._controls.viewSelector._onTextChanged = function(se, prop) {};
+}
+
+function disable() {
+	Main.overview._searchEntry.show();
+	Main.overview._controls.viewSelector.startSearch = this.SavedstartSearch;
+	Main.overview._controls.viewSelector._onTextChanged = this.SavedonTextChanged;
+}
